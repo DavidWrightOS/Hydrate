2f0fa20d84ba600dfa1a1788f761e773716edfd1
David Wright <davidtwright89@gmail.com>
2020-09-19 13:05:36 -0400

    Added a DataViewController to the project

---
+++ Hydrate/View Controllers/DataViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DataViewController.swift
+//  Hydrate
+//
+//  Created by David Wright on 9/19/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+    
+}
