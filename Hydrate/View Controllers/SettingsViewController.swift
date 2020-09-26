aece95ed058e16068ed4703149ebf9f7ce66f7b0
David Wright <davidtwright89@gmail.com>
2020-09-26 16:36:46 -0400

    Added a SettingsViewController class

---
+++ Hydrate/View Controllers/SettingsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsViewController.swift
+//  Hydrate
+//
+//  Created by David Wright on 9/26/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    
+}
