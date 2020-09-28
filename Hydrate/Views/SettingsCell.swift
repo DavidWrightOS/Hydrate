9d719d1c884b5388dd746a6d0850dafbdff842c8
David Wright <davidtwright89@gmail.com>
2020-09-28 03:13:00 -0400

    Added a custom SettingsCell class for the settings tableView

---
+++ Hydrate/Views/SettingsCell.swift
@@ -0,0 +1,65 @@
+//
+//  SettingsCell.swift
+//  Hydrate
+//
+//  Created by David Wright on 9/28/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    var setting: SettingOption? {
+        didSet {
+            guard let setting = setting else { return }
+            textLabel?.text = setting.description
+            switchControl.isHidden = !setting.containsSwitch
+            
+            if setting.containsSwitch {
+                self.accessoryType = .none
+            } else {
+                addDisclosureIndicator()
+            }
+        }
+    }
+    
+    lazy var switchControl: UISwitch = {
+        let switchControl = UISwitch()
+        switchControl.isOn = true
+        switchControl.onTintColor = .sicklySmurfBlue
+        switchControl.translatesAutoresizingMaskIntoConstraints = false
+        switchControl.addTarget(self, action: #selector(handleSwitchAction), for: .valueChanged)
+        return switchControl
+    }()
+    
+    // MARK: - Init
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        selectionStyle = .none
+        backgroundColor = .ravenClawBlue90
+        textLabel?.textColor = .undeadWhite
+        
+        addSubview(switchControl)
+        switchControl.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        switchControl.rightAnchor.constraint(equalTo: rightAnchor, constant: -12).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleSwitchAction(sender: UISwitch) {
+        if sender.isOn {
+            print("Debug: \(setting?.description ?? "?") turned ON..")
+        } else {
+            print("Debug: \(setting?.description ?? "?") turned OFF..")
+        }
+    }
+}
