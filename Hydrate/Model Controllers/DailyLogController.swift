4d0c8a8ec8d71c3c8e827f0dc9d701d0a702503e
David Wright <davidtwright89@gmail.com>
2020-10-02 01:24:47 -0400

    Added a DailyLogController model controller class

---
+++ Hydrate/Model Controllers/DailyLogController.swift
@@ -0,0 +1,14 @@
+//
+//  DailyLogController.swift
+//  Hydrate
+//
+//  Created by David Wright on 10/2/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DailyLogController {
+    
+}
