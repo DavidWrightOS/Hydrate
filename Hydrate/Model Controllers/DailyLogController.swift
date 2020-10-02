78684fbe2e703ae3cd37858d920ae896646e8894
David Wright <davidtwright89@gmail.com>
2020-10-02 01:33:53 -0400

    Added a dailyLog(for date:) method to the DailyLogController

--- Hydrate/Model Controllers/DailyLogController.swift
+++ Hydrate/Model Controllers/DailyLogController.swift
@@ -11,4 +11,31 @@ import CoreData
 
 class DailyLogController {
     
+    // MARK: - Properties
+    
+    lazy var coreDataStack = CoreDataStack.shared
+    
+    // MARK: - Methods
+    
+    fileprivate func dailyLog(for date: Date = Date()) -> DailyLog {
+        let day = date.startOfDay
+        let fetchRequest: NSFetchRequest<DailyLog> = DailyLog.fetchRequest()
+        let datePredicate = NSPredicate(format: "(%K = %@)", #keyPath(DailyLog.date), day as NSDate)
+        fetchRequest.predicate = datePredicate
+        
+        do {
+            let dailyLog = try CoreDataStack.shared.mainContext.fetch(fetchRequest).first
+            if let dailyLog = dailyLog {
+                return dailyLog
+            }
+        } catch let error as NSError {
+            print("Error fetching: \(error), \(error.userInfo)")
+        }
+        
+        let dailyLog = DailyLog(date: day)
+        coreDataStack.saveContext()
+        
+        return dailyLog
+    }
+    
 }
