caa83e7c105f56c962c5306442ad909ca6745fbd
David Wright <davidtwright89@gmail.com>
2020-10-23 02:31:07 -0400

    Added a MeasurementMarkerView class in order to help cleanup the massive MainViewController

---
+++ Hydrate/Views/MeasurementMarkerView.swift
@@ -0,0 +1,15 @@
+//
+//  MeasurementMarkerView.swift
+//  Hydrate
+//
+//  Created by David Wright on 10/23/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class MeasurementMarkerView: UIView {
+    
+    
+}
+    
